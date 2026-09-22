31e2adc04601f81860ab76cb4a096658af89ccec
Jim Beebe <jim_beebe@jimbeebewriter.com>
2026-09-22 10:12:10 -0700

    Removed edit links for the config file.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,11 +39,11 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/Jim-Beebe-Writer/Website/tree/main/',
+          // editUrl: 'https://github.com/Jim-Beebe-Writer/Website/tree/main/',
         },
         blog: {
           showReadingTime: true,
-          editUrl: 'https://github.com/Jim-Beebe-Writer/Website/tree/main/',
+          // editUrl: 'https://github.com/Jim-Beebe-Writer/Website/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
